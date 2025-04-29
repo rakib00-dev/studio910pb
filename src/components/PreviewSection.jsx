a5a36f3564e4672e61997fbb69f565e8a559faee
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 16:42:22 +0600

    added section called PreviweSection

---
+++ src/components/PreviewSection.jsx
@@ -0,0 +1,14 @@
+const PreviewSection = () => {
+  return (
+    <section className="">
+      <div className="relative overflow-hidden w-full pt-[56.25%]">
+        <iframe
+          src="https://player.vimeo.com/video/855018152?muted=1&autoplay=1&loop=1&background=1&app_id=122963"
+          className="absolute inset-0 w-full h-full"
+        ></iframe>
+      </div>
+    </section>
+  );
+};
+
+export default PreviewSection;
