59540fa242373063d0fa985505bcf571eb12ff26
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 14:57:45 +0600

    added pinnacle section and finished the work of the section

--- src/components/Pinnacle.jsx
+++ src/components/Pinnacle.jsx
@@ -1,16 +1,31 @@
+import { FaGreaterThan } from 'react-icons/fa';
+import { IoIosArrowForward } from 'react-icons/io';
+
 const Pinnacle = () => {
   return (
     <section className="relative sectionWidth pinnacleSection w-fit h-full">
-      <div className="wrapper py-20">
-        <div className="absolute inset-0 bg-gray-800/50 bg-opacity-20"></div>
-        <div className="relative z-10">
-          <h1 className="text-center text-[2.4rem] md:text-[4rem] font-light">
+      <div className="absolute inset-0 bg-[#22211da7]"></div>
+      <div className="wrapper py-30 grid md:grid-cols-2 w-full">
+        <div className="relative z-10 uppercase">
+          <h1 className="text-[1.4rem] md:text-[2.5rem] font-light leading-12">
             This is the{' '}
-            <span style={{ color: 'var(--primary-blue)' }}>pinnacle</span> of
-            real estate media.
+            <span style={{ color: 'var(--primary-blue)' }}>pinnacle</span>
+            <br /> of real estate media.
           </h1>
         </div>
-        <div></div>
+        <div className="grid z-10 pl-5 w-full gap-6">
+          <p className="text-xl  text-[#bbb]">
+            Discover the world of what real estate media was meant to be. Our
+            work speaks for itself.
+          </p>
+
+          <a
+            href="https://studio910pb.com/portfolio/"
+            className="transition-all duration-300 text-white hover:text-[#bbb] capitalize flex gap-2 items-center border-b py-2 w-fit jumpHover"
+          >
+            our portfolio <IoIosArrowForward />
+          </a>
+        </div>
       </div>
     </section>
   );
