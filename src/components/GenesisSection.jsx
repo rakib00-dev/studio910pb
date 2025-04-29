2f37c302deff86a8203ac1778bd216ef3a802b9c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 15:14:15 +0600

    added new section called Genesis

---
+++ src/components/GenesisSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GenesisSection = () => {
+  return (
+    <section className="sectionWidth">
+      <div className="wrapper"></div>
+    </section>
+  );
+};
+
+export default GenesisSection;
