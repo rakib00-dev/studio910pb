7a5523133274a11191ce89c68ee97d8596f3b2eb
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 09:26:40 +0600

    updated the hero text color with variable

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ const Hero = () => {
         {/* text content */}
         <div className="pt-5 pb-20 grid gap-5 uppercase">
           <h1 className="text-center text-[2.4rem] md:text-[4rem] font-light">
-            Your <span className="text-[#38b6ff]">bespoke</span> partner.
+            Your <span style={{ color: 'var(--primary-blue)' }}>bespoke</span>{' '}
+            partner.
           </h1>
           <h3 className="text-center text-[1.8rem] md:text-[2.5rem] font-light">
             Luxury real estate media to impress
