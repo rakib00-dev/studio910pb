1ac2f23c9ecac82db155e57dd28977e70bee7430
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 23:46:09 +0600

    added the footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="sectionWidth">
-      <div className="wrapper py-20">
+    <footer className="">
+      <div className="py-20 overflow-hidden">
         {/* wait list */}
-        <div className="grid place-items-center gap-5">
+        <div className="wrappergrid place-items-center gap-5 pb-10 md:pb-20">
           <h1 className="text-center text-[2.4rem] md:text-[4rem] uppercase ">
             The 10 Laws of Branding
           </h1>
@@ -14,7 +14,10 @@ const Footer = () => {
             <span className="italic">exclusive</span> ’10 Laws of Branding.’
             Transform your brand into a force to be reckoned with.
           </p>
-          <form action="" className="flex w-full gap-5">
+          <form
+            action=""
+            className="wrapper flex flex-col md:flex-row w-full gap-5"
+          >
             <div className="w-full">
               <label htmlFor="name" className="uppercase">
                 Name
@@ -41,14 +44,21 @@ const Footer = () => {
                 required
               />
             </div>
-            <a
-              href="https://studio910pb.com/about/"
-              className="transtion-all duration-300 uppercase flex justify-start py-3 px-6 border font-medium jumpHover hover:bg-white hover:text-black w-fit"
+            <button
+              type="submit"
+              className="transtion-all duration-300 flex justify-center relative uppercase py-3 px-2 border font-medium jumpHover hover:bg-white hover:text-black md:w-[36rem] h-full text-center cursor-pointer md:top-8"
             >
-              about studio 910
-            </a>
+              join wait list
+            </button>
           </form>
         </div>
+        {/* water fall view */}
+        <img
+          src="/images/footer/24-web-or-mls-DSC09153.jpg"
+          alt=""
+          loading="lazy"
+          className="relative w-full bottom-0"
+        />
       </div>
     </footer>
   );
