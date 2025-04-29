c20d66747848652d7001989c4a2a091c4f13063d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 15:01:09 +0600

    updated the pinnacle section

--- src/components/Pinnacle.jsx
+++ src/components/Pinnacle.jsx
@@ -5,15 +5,15 @@ const Pinnacle = () => {
   return (
     <section className="relative sectionWidth pinnacleSection w-fit h-full">
       <div className="absolute inset-0 bg-[#22211da7]"></div>
-      <div className="wrapper py-30 grid md:grid-cols-2 w-full">
+      <div className="wrapper py-15 md:py-30 grid md:grid-cols-2 w-full gap-10">
         <div className="relative z-10 uppercase">
-          <h1 className="text-[1.4rem] md:text-[2.5rem] font-light leading-12">
+          <h1 className="text-[1.8rem] md:text-[2.5rem] font-light leading-8 md:leading-12">
             This is the{' '}
             <span style={{ color: 'var(--primary-blue)' }}>pinnacle</span>
             <br /> of real estate media.
           </h1>
         </div>
-        <div className="grid z-10 pl-5 w-full gap-6">
+        <div className="grid z-10 md:pl-5 w-full gap-6">
           <p className="text-xl  text-[#bbb]">
             Discover the world of what real estate media was meant to be. Our
             work speaks for itself.
