d663a60a7b0808b476bf0949fd0bf9b56e59132c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 20:52:04 +0600

    added some more in the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className="sectionWidth">Footer</footer>;
+  return (
+    <footer className="sectionWidth">
+      <div className="wrapper py-20">
+        {/* wait list */}
+        <div className="grid place-items-center gap-5">
+          <h1 className="text-center text-[2.4rem] md:text-[4rem] uppercase ">
+            The 10 Laws of Branding
+          </h1>
+          <p className="uppercase text-xl text-center w-[95%] md:w-1/2">
+            Sign Up and we’ll send you a copy of the{' '}
+            <span className="italic">exclusive</span> ’10 Laws of Branding.’
+            Transform your brand into a force to be reckoned with.
+          </p>
+          <form action="" className="flex w-full gap-5">
+            <div className="w-full">
+              <label htmlFor="name" className="uppercase">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+                className="border-b px-2 py-4 w-full outline-none"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label htmlFor="email" className="uppercase">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                className="border-b px-2 py-4 w-full outline-none"
+                required
+              />
+            </div>
+            <a
+              href="https://studio910pb.com/about/"
+              className="transtion-all duration-300 uppercase flex justify-start py-3 px-6 border font-medium jumpHover hover:bg-white hover:text-black w-fit"
+            >
+              about studio 910
+            </a>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
