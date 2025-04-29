1a09c280a847595f9a422a675a3df1d19724313c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 08:53:32 +0600

    added navbar list hover style and hero hover style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,12 @@ const Hero = () => {
         {/* text content */}
         <div></div>
         {/* video content */}
-        <div>
+        <div className="w-full">
           <iframe
             src="https://player.vimeo.com/video/855018152?muted=1&autoplay=1&loop=1&background=1&app_id=122963"
-            frameborder="0"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+            className="heroVideo"
           ></iframe>
         </div>
       </div>
