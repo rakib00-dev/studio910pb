4d77a9de83fc6a5b51c133aa99b2751fb90c852a
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 20:36:03 +0600

    added swimmingPool section

--- src/components/SwimmingPool.jsx
+++ src/components/SwimmingPool.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const SwimmingPool = () => {
   return (
     <section className="">
-      <div className="wrapper">
-        <div className="swimmingPool"></div>
-      </div>
+      <div className="swimmingPool"></div>
     </section>
   );
 };
