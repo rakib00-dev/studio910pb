7f103634c45e772277f80c2471fc57e1cd021230
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 17:41:40 +0600

    finished the discerning section

--- src/components/DiscerningSection.jsx
+++ src/components/DiscerningSection.jsx
@@ -1,13 +1,33 @@
 const DiscerningSection = () => {
   return (
     <section className="sectionWidth">
-      <div className="wrapper">
-        <div>
-          <h2>
+      <div className="wrapper py-20 grid md:grid-cols-2 overflow-hidden gap-10">
+        <div className="flex justify-center items-center">
+          <h2 className="text-center md:text-start text-[1.5rem] md:text-[2.8rem] font-light leading-8 md:leading-12 uppercase">
             What our{' '}
             <span style={{ color: 'var(--primary-blue)' }}>discerning</span>{' '}
             clients are saying...
           </h2>
+        </div>
+        <div className="grid grid-cols-2 place-items-center md:grid-cols-3 gap-5 px-2">
+          <img
+            src="/images/discerning/DSC01964-Edit-2-3-683x1024.jpg"
+            alt=""
+            loading="lazy"
+            className=" w-full"
+          />
+          <img
+            src="/images/discerning/DSC07186-2-683x1024.jpg"
+            alt=""
+            loading="lazy"
+            className=" w-full"
+          />
+          <img
+            src="/images/discerning/DSC05858-683x1024.jpg"
+            alt=""
+            loading="lazy"
+            className=" w-full col-span-2 md:col-span-1"
+          />
         </div>
       </div>
     </section>
