6443d72dd27bbedf63f7e12d24f84111957086b4
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 16:41:56 +0600

    bring back hero section content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,12 @@ const Hero = () => {
           </h3>
         </div>
         {/* video content */}
+        <div className="relative overflow-hidden w-full pt-[56.25%]">
+          <iframe
+            src="https://player.vimeo.com/video/855018152?muted=1&autoplay=1&loop=1&background=1&app_id=122963"
+            className="absolute inset-0 w-full h-full"
+          ></iframe>
+        </div>
       </div>
     </section>
   );
