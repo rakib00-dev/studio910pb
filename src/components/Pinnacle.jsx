6b7495df8a130eb95eecfc114fb9529f588b827f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-04-29 14:36:29 +0600

    added pinnacle section img and fix it

--- src/components/Pinnacle.jsx
+++ src/components/Pinnacle.jsx
@@ -1,7 +1,17 @@
 const Pinnacle = () => {
   return (
-    <section className="sectionWidth pinnacleSection">
-      <div className="wrapper">asdf</div>
+    <section className="relative sectionWidth pinnacleSection w-fit h-full">
+      <div className="wrapper py-20">
+        <div className="absolute inset-0 bg-gray-800/50 bg-opacity-20"></div>
+        <div className="relative z-10">
+          <h1 className="text-center text-[2.4rem] md:text-[4rem] font-light">
+            This is the{' '}
+            <span style={{ color: 'var(--primary-blue)' }}>pinnacle</span> of
+            real estate media.
+          </h1>
+        </div>
+        <div></div>
+      </div>
     </section>
   );
 };
